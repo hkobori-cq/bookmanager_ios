bc2e5819ba1431e689902b0fd007ab5721e87409
hkobori-cq <h.kobori@caraquri.com>
2016-08-30 15:45:07 +0900

    ユーザー登録画面のlayout完成

--- BookManager/Controllers/AccountSettingsViewController.m
+++ BookManager/Controllers/AccountSettingsViewController.m
@@ -1,9 +1,10 @@
 #import "AccountSettingsViewController.h"
 
-@interface AccountSettingsViewController ()
+@interface AccountSettingsViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *mailBox;
 @property (weak, nonatomic) IBOutlet UITextField *passwordBox;
 @property (weak, nonatomic) IBOutlet UITextField *passwordConfirmBox;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -15,12 +16,50 @@
     // Do any additional setup after loading the view.
     self.navigationController.navigationBar.tintColor = [UIColor blackColor];
     self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    self.mailBox.delegate = self;
+    self.passwordBox.delegate = self;
+    self.passwordConfirmBox.delegate = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasHidden:)
+                                                 name:UIKeyboardDidHideNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/**
+ * keyboardが出ているとき別の場所をタップしたときの動作
+ */
+- (IBAction)onSingleTap:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.mailBox resignFirstResponder];
+    [self.passwordBox resignFirstResponder];
+    [self.passwordConfirmBox resignFirstResponder];
+    return YES;
+}
+
+- (void)keyboardWasShown:(NSNotification *)notification {
+    NSDictionary *info  = [notification userInfo];
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    CGPoint scrollPoint = CGPointMake(0.0f, keyboardSize.height/3);
+    [self.scrollView setContentOffset:scrollPoint animated:YES];
+}
+
+- (void)keyboardWasHidden:(NSNotification *)notification {
+    [self.scrollView setContentOffset:CGPointMake(0.0f, 80.0f) animated:YES];
+}
+
 
 /*
 #pragma mark - Navigation
