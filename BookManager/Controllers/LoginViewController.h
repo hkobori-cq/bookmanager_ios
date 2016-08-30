03bd7a151acefa50685f5e13fbf6f722e911ac82
hkobori-cq <h.kobori@caraquri.com>
2016-08-30 23:50:49 +0900

    loginViewの設置

---
+++ BookManager/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  BookManager
+//
+//  Created by 小堀輝 on 2016/08/30.
+//  Copyright © 2016年 hikaru kobori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ BookManager/Controllers/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  BookManager
+//
+//  Created by 小堀輝 on 2016/08/30.
+//  Copyright © 2016年 hikaru kobori. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *mailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)loginButton:(id)sender {
+}
+- (IBAction)addUserButton:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
